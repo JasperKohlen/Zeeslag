6ed0f1f18a48e9f0ce8b9997b83e8cbd8c19ccab
jkohlen <386329@student.fontys.nl>
2020-10-05 11:29:00 +0200

    added logincontroller

---
+++ seabattlerest/src/main/java/com/example/rest/AuthController.java
@@ -0,0 +1,19 @@
+package com.example.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+//import javax.validation.Valid;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class AuthController {
+//    @Autowired
+//    private UserService userService;
+
+    @GetMapping(value = "login")
+    public void Login (){//(@Valid @RequestBody AuthorisationModel authModel){
+
+    }
+}
