e6ea6270fa2242712365ad95759f9e051a75bdd8
jkohlen <386329@student.fontys.nl>
2020-10-27 10:38:06 +0100

    .

---
+++ seabattleserver/src/main/java/WebSocket/WebsocketServer.java
@@ -0,0 +1,43 @@
+package WebSocket;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer;
+
+
+import javax.websocket.server.ServerContainer;
+
+public class WebsocketServer {
+    private static void startWebSocketServer() {
+        Server webSocketServer = new Server();
+        ServerConnector connector = new ServerConnector(webSocketServer);
+        connector.setPort(8095);
+        webSocketServer.addConnector(connector);
+
+        // Setup the basic application "context" for this application at "/"
+        // This is also known as the handler tree (in jetty speak)
+        ServletContextHandler webSocketContext = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        webSocketContext.setContextPath("/");
+        webSocketServer.setHandler(webSocketContext);
+
+        try {
+            // Initialize javax.websocket layer
+            ServerContainer wscontainer = WebSocketServerContainerInitializer.configureContext(webSocketContext);
+
+            // Add WebSocket endpoint to javax.websocket layer
+            wscontainer.addEndpoint(CommunicatorWebSocket.class);
+
+            webSocketServer.start();
+            //server.dump(System.err);
+
+            webSocketServer.join();
+        } catch (Throwable t) {
+            t.printStackTrace(System.err);
+        }
+    }
+
+    public static void main(String[] args) {
+        startWebSocketServer();
+    }
+}
--- seabattleserver/src/main/java/module-info.java
+++ seabattleserver/src/main/java/module-info.java
@@ -1,4 +1,9 @@
 module seabattleserver {
     requires javax.websocket.client.api;
-    requires slf4j.api; // logging with Logback
+    requires slf4j.api;
+    requires javax.websocket.api;
+    requires org.eclipse.jetty.servlet;
+    requires org.eclipse.jetty.server;
+    requires org.eclipse.jetty.websocket.javax.websocket;
+    requires gson; // logging with Logback
 }
